6ab79240e6e70cebf5dc961b7f2cc21aecd139cc
Ming <mjing3@my.bcit.ca>
2017-04-24 14:11:44 -0700

    Basic Setup

---
+++ KoreApiServer/KoreApiServer/Controllers/UploadController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace KoreApiServer.Controllers
+{
+    public class UploadController : ApiController
+    {
+        // The first parameter restricts the domains that can access
+        // this service. In this case it is open to all.
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+
+
+        // GET: api/Upload
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Upload/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Upload
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Upload/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Upload/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
